85bb9ccc9b98b48b6ccda4870aec93b41f1781db
sahil sarawagi <sahilsarawagi1@gmail.com>
2024-02-21 23:41:47 +0530

    -Implemented Kitten model, CRUD actions, and basic views - Added delete links and flash messages

---
+++ app/controllers/kittens_controller.rb
@@ -0,0 +1,46 @@
+class KittensController < ApplicationController
+  def index
+    @kittens = Kitten.all
+    flash.now[:notice] = "We have exactly #{@kittens.size} kittens registered."
+  end
+  def new
+    @kitten = Kitten.new 
+  end
+  def create
+    @kitten = Kitten.new(kitten_params)
+
+    if @kitten.save
+      redirect_to @kitten , notice: "Congrats, you have successfully registered your cat"
+    else
+      render :new, status: :unprocessable_entity, notice: "Error, something went wrong"
+    end
+  end
+
+  def show
+    @kitten = Kitten.find(params[:id])
+  end
+
+  def edit
+    @kitten = Kitten.find(params[:id])
+  end
+
+  def update
+    @kitten = Kitten.find(params[:id])
+
+    if @kitten.update(kitten_params)
+      redirect_to @kitten , notice: "Congrats, you have successfully updated information of your cat"
+    else
+      render :edit, status: :unprocessable_entity , notice: "Error, something went wrong"
+    end
+  end
+  def destroy
+    @kitten = Kitten.find(params[:id])
+    @kitten.destroy
+
+    redirect_to root_path, status: :see_other, notice: "You have successfully deleted your kitten"
+  end
+  private
+    def kitten_params
+      params.require(:kitten).permit(:name, :age , :cuteness , :softness)
+    end
+end
